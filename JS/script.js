fcf268fdc4bd353380155035a2c4d5be291c6b3d
Quentinx6 <quentin.lecompte@3wa.io>
2021-07-08 11:02:18 +0200

    script

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ var button_bateau1 = document.getElementsByClassName("button--bateau1")[0];
 var button_bateau2 = document.getElementsByClassName("button--bateau2")[0];
 var button_bateau3= document.getElementsByClassName("button--bateau3")[0];
 var button_bateau4 = document.getElementsByClassName("button--bateau4")[0];
+var button_Quentin = document.getElementsByClassName("button--Quentin")[0];
 
 button_basic.onclick = function() {
 	tangram.className =
@@ -22,6 +23,10 @@ button_bateau3.onclick = function() {
   tangram.className.replace(/tangram--(\S*)/,'tangram--bateau3');
 };
 button_bateau4.onclick = function() {
-	tangram.className =
-  tangram.className.replace(/tangram--(\S*)/,'tangram--bateau4');
+	tangram.className =tangram.className.replace(/tangram--(\S*)/,'tangram--bateau4');
+
+};
+button_Quentin.onclick = function() {
+	tangram.className =tangram.className.replace(/tangram--(\S*)/,'tangram--Quentin');
+
 };
