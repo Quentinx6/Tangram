19292ad0c208235e601b41374b9cf35f3d4cefc4
kevmaillard <kemaillard@gmail.com>
2021-07-09 12:21:58 +0200

    def

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,9 @@ var button_bateau2 = document.getElementsByClassName("button--bateau2")[0];
 var button_bateau3= document.getElementsByClassName("button--bateau3")[0];
 var button_bateau4 = document.getElementsByClassName("button--bateau4")[0];
 var button_soufiane = document.getElementsByClassName("button--soufiane")[0];
+var button_kevin = document.getElementsByClassName("button--kevin")[0];
+var button_Quentin = document.getElementsByClassName("button--Quentin")[0];
+var button_Simon = document.getElementsByClassName("button--Simon")[0];
 
 
 button_basic.onclick = function() {
@@ -27,7 +30,19 @@ button_bateau4.onclick = function() {
 	tangram.className =
   tangram.className.replace(/tangram--(\S*)/,'tangram--bateau4');
 };
-button_bateau4.onclick = function() {
+button_soufiane.onclick = function() {
 	tangram.className =
   tangram.className.replace(/tangram--(\S*)/,'tangram--soufiane');
+};
+button_Quentin.onclick = function() {
+	tangram.className =
+  tangram.className.replace(/tangram--(\S*)/,'tangram--Quentin');
+};
+button_Simon.onclick = function() {
+	tangram.className =
+  tangram.className.replace(/tangram--(\S*)/,'tangram--Simon');
+};
+button_kevin.onclick = function() {
+	tangram.className =
+  tangram.className.replace(/tangram--(\S*)/,'tangram--kevin');
 };
