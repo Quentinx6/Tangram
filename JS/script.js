1b894c3fa5f0a126223e52109be2ac0dddfdb2ba
soufianekkk <sou.kabdani@gmail.com>
2021-07-09 11:59:55 +0200

    commit

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,8 @@ var button_bateau1 = document.getElementsByClassName("button--bateau1")[0];
 var button_bateau2 = document.getElementsByClassName("button--bateau2")[0];
 var button_bateau3= document.getElementsByClassName("button--bateau3")[0];
 var button_bateau4 = document.getElementsByClassName("button--bateau4")[0];
+var button_soufiane = document.getElementsByClassName("button--soufiane")[0];
+
 
 button_basic.onclick = function() {
 	tangram.className =
@@ -24,4 +26,8 @@ button_bateau3.onclick = function() {
 button_bateau4.onclick = function() {
 	tangram.className =
   tangram.className.replace(/tangram--(\S*)/,'tangram--bateau4');
+};
+button_bateau4.onclick = function() {
+	tangram.className =
+  tangram.className.replace(/tangram--(\S*)/,'tangram--soufiane');
 };
